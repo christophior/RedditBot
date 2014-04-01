69d186f7a0852fc13faf55e2039ce13dee7a574e
Chris Villarreal <chrisvillarreal1018@gmail.com>
2014-04-01 14:35:30 -0500

    hardcoding test login info

--- RedditBot/RedditBot.py
+++ RedditBot/RedditBot.py
@@ -8,19 +8,19 @@ if __name__ == "__main__":
     print "starting RedditBot"
 
     # Retrieve heroku env variables for deployment
-    login_info = [os.environ['REDDIT_USERNAME'], os.environ['REDDIT_PASSWORD']]
+    # login_info = [os.environ['REDDIT_USERNAME'], os.environ['REDDIT_PASSWORD']]
 
     # bot login info for local testing
     # with open("login.properties", "r") as loginFile:
         # login_info = loginFile.readlines()
 
     # removing new lines from username/password
-    login_info[0] = login_info[0].replace('\n', '')
-    login_info[1] = login_info[1].replace('\n', '')
+    # login_info[0] = login_info[0].replace('\n', '')
+    # login_info[1] = login_info[1].replace('\n', '')
 
     # set up praw
     r = praw.Reddit('christophiorbot')
-    r.login(login_info[0], login_info[1])
+    r.login('christophiorbot', 'test')
 
     # USER="christophior"
     # print "Sending message"
