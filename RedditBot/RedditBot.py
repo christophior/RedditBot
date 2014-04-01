4a4ace8dc979b0ef8460f1d3f2c4952e6202c820
Chris Villarreal <chrisvillarreal1018@gmail.com>
2014-04-01 12:06:23 -0500

    updated login retrieval for heroku and updated ignore file

--- RedditBot/RedditBot.py
+++ RedditBot/RedditBot.py
@@ -1,17 +1,21 @@
 import praw
 import pickle
 import time
+import os
 
-# bot login info
-with open("login.properties", "r") as loginFile:
-    login_info = loginFile.readlines()
+# bot login info for local testing
+# with open("login.properties", "r") as loginFile:
+#     login_info = loginFile.readlines()
 
-# zeroing out login info
+# Retrieve heroku env variables for deployment
+login_info = [os.environ['REDDIT_USERNAME'], os.environ['REDDIT_PASSWORD']]
+
+# removing new lines from username/password
 login_info[0] = login_info[0].replace('\n', '')
 login_info[1] = login_info[1].replace('\n', '')
 
 # set up praw
-r = praw.Reddit('trollbot')
+r = praw.Reddit('christophiorbot')
 r.login(login_info[0], login_info[1])
 
 # USER="christophior"
